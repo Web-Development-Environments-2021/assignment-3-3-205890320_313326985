76ec86742707cf64ec98c1d666d249ad3d25d743
liadna1995 <liadna1995@gmail.com>
2021-06-21 19:02:17 +0300

    shows all favorite matches, not specific to a user

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -2,20 +2,20 @@
   <div class="container">
     <h1 class="title">Main Page</h1>
     <LoginPage v-if="!$root.store.username"></LoginPage>
-    <FavoriteGames v-else></FavoriteGames>
+    <FavoriteMatches v-else></FavoriteMatches>
     <LeagueInfo></LeagueInfo>
   </div>
 </template>
 
 <script>
 import LeagueInfo from "../components/LeagueInfo";
-import FavoriteGames from "../components/FavoriteGames";
+import FavoriteMatches from "../components/FavoriteMatches";
 import LoginPage from "../pages/LoginPage";
 export default {
   components: {
     LeagueInfo, 
     LoginPage, 
-    FavoriteGames
+    FavoriteMatches
   }
 };
 </script>
