b8bacf8da2f87ed8b2af1fb9558d384f8cd586a1
liadna1995 <liadna1995@gmail.com>
2021-07-10 15:05:10 +0300

    converted future matches in stageMatches

--- src/components/TableMatchFuturePreview.vue
+++ src/components/TableMatchFuturePreview.vue
@@ -1,39 +1,71 @@
 <template >
   <div>
-    <table class="table table-striped table-hover">
-      <thead>
-        <tr>
-          <th scope="col">#id </th>
-          <th scope="col">Date </th>
-          <th scope="col">Time</th>
-          <th scope="col">Local Team </th>
-          <th scope="col">Visitor Team </th>
-          <th scope="col">Venue</th>
-          <th scope="col">Referee</th>
-          <th scope="col"></th>
-          <th scope="col"></th>
-          <th scope="col"></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="match in futureLeagueMatches" :key="match.match_id">
-          <th scope="row">{{match.match_id}}</th>
-          <td> {{match.date_time.slice(0,10)}} </td>
-          <td> {{match.date_time.slice(11,16)}} </td>
-          <td> {{match.local_team_name}} </td>
-          <td> {{match.visitor_team_name}} </td>
-          <td> {{match.venue_name}} </td>
-          <td> {{match.referee_id}} </td>
-          <td> <button v-if="$root.store.username" :disabled="FavorMatch(match.match_id)" @click="addMatchToFavorites(match.match_id)" type="button" class="btn btn-outline-secondary btn-sm">add to favorites</button> </td>
-        </tr>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      aria-controls="my-table"
+    ></b-pagination>
 
-      </tbody>
-    </table>
+    <p class="mt-3">Current Page: {{ currentPage }}</p>
+
+    <b-container fluid>
+    <b-table 
+      striped
+      hover
+      :items="futureLeagueMatches"
+      :fields="fields"
+      :current-page="currentPage"
+      :per-page="perPage"
+      stacked="md"
+      show-empty
+      small
+    >
++
+
+
+    <template #cell(date_time)="row">
+        {{ row.value.substr(0,16) }}
+      </template>
+
+
+    <template #cell(actions)="row">
+      <button v-if="$root.store.username" :disabled="FavorMatch(row.item.match_id)" @click="addMatchToFavorites(match.match_id)" type="button" class="btn btn-outline-secondary btn-sm">add to favorites</button>
+      </template>
+
+
+  </b-table>
+  </b-container>
+
+
+
+
+
   </div>
 </template>
 
 <script>
 export default {
+  data(){
+    return{
+      items: [],
+      fields: [
+        {key: 'match_id', label: '#MatchId'},
+        {key: 'date_time', label: 'Date and Time'},
+        {key: 'local_team_name', label: 'Local team'},
+        {key: 'visitor_team_name', label: 'Visitor team'},
+        {key: 'venue_name', label: 'Venue'},
+        {key: 'actions', label: 'Actions' }        
+      ],
+      perPage: 3,
+      currentPage: 1
+    }
+  },
+  computed:{
+    rows() {
+        return this.futureLeagueMatches.length;
+      }
+  },
   props:{
     futureLeagueMatches:{
       required: true,
@@ -87,6 +119,35 @@ export default {
       },
   }
 };
+    // <!-- <table class="table table-striped table-hover">
+    //   <thead>
+    //     <tr>
+    //       <th scope="col">#id </th>
+    //       <th scope="col">Date </th>
+    //       <th scope="col">Time</th>
+    //       <th scope="col">Local Team </th>
+    //       <th scope="col">Visitor Team </th>
+    //       <th scope="col">Venue</th>
+    //       <th scope="col">Referee</th>
+    //       <th scope="col"></th>
+    //       <th scope="col"></th>
+    //       <th scope="col"></th>
+    //     </tr>
+    //   </thead>
+    //   <tbody>
+    //     <tr v-for="match in futureLeagueMatches" :key="match.match_id">
+    //       <th scope="row">{{match.match_id}}</th>
+    //       <td> {{match.date_time.slice(0,10)}} </td>
+    //       <td> {{match.date_time.slice(11,16)}} </td>
+    //       <td> {{match.local_team_name}} </td>
+    //       <td> {{match.visitor_team_name}} </td>
+    //       <td> {{match.venue_name}} </td>
+    //       <td> {{match.referee_id}} </td>
+    //       <td> <button v-if="$root.store.username" :disabled="FavorMatch(match.match_id)" @click="addMatchToFavorites(match.match_id)" type="button" class="btn btn-outline-secondary btn-sm">add to favorites</button> </td>
+    //     </tr>
+
+    //   </tbody>
+    // </table> -->
 </script>
 
 <style>
